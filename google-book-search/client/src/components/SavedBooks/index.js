e14264b7d31a2a144d118d74dd52688aae958bd3
Daniel Duran Jr <danielduranjr@Daniels-Air.attlocal.net>
2020-06-07 22:10:35 -0500

    deploy to heroku

--- google-book-search/client/src/components/SavedBooks/index.js
+++ google-book-search/client/src/components/SavedBooks/index.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import BooksCard from "../BooksCard";
+import API from "../../utils/API";
 
 function SavedBooks() {
+    const [books, setBooks] = useState([]);
+    useEffect(()=> {
+        API.getBooks().then(res => setBooks(res.data))
+    },[])
   return (
     <div>
       <h1>Saved Books</h1>
      
-      <BooksCard type="saved"/>
+      {books && books.length > 0
+        ? books.map((book) => {
+            return (
+              <BooksCard
+                type="saved"
+                key={book.id}
+                title={book.volumeInfo.title}
+                authors={book.volumeInfo.authors[0]}
+                description={book.volumeInfo.description}
+                imageLinks={book.volumeInfo.imageLinks.smallThumbnail}
+                infoLink={book.volumeInfo.infoLink}
+              />
+            );
+          })
+        : ""}
     </div>
   );
 }
