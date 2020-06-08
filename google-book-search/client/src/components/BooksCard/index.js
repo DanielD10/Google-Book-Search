a112ab94baecde729f491ec5946ab1be35ed95d1
Daniel Duran Jr <danielduranjr@Daniels-Air.attlocal.net>
2020-06-07 22:01:10 -0500

    adding handleEvents and onlick

--- google-book-search/client/src/components/BooksCard/index.js
+++ google-book-search/client/src/components/BooksCard/index.js
@@ -1,32 +1,56 @@
 import React from "react";
+import API from "../../utils/API";
 
 function BookCard(props) {
+  function saveBook() {
+    API.saveBook({
+      title: props.title,
+      authors: props.authors,
+      description: props.description,
+      image: props.imageLinks,
+      link: props.infoLink,
+    }).then(() => console.log("Book Saved!"))
+    .catch(err => console.log(err));
+    
+  }
+
+  function deleteBook(id) {
+      API.deleteBook(id).then(() => console.log("Book Deleted!"))
+      .catch(err => console.log(err));
+
+  }
+
   return (
     <div>
       <div
         className="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin"
         uk-grid="true"
       >
+        <a href={props.infoLink}>
+          {" "}
           <button className="uk-button uk-button-default">Veiw</button>
-          {props.type === "search" ? <button className="uk-button uk-button-default">Add</button> :
-          <button  className="uk-button uk-button-default">Delete</button>}
+        </a>
+
+        {props.type === "search" ? (
+          <button onClick={saveBook} className="uk-button uk-button-default">
+            Add
+          </button>
+        ) : (
+          <button onClick={deleteBook} className="uk-button uk-button-default">Delete</button>
+        )}
         <div className="uk-card-media-left uk-cover-container">
-          <img src="images/light.jpg" alt="" uk-cover="true" />
+          <img src={props.imageLinks} alt="" uk-cover="true" />
           <canvas width="600" height="400"></canvas>
         </div>
 
         <div>
           <div className="uk-card-body">
-            <h3 className="uk-card-title">Media Left</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt.
-            </p>
+            <h1 className="uk-card-title">{props.title}</h1>
+            <h3> {props.authors} </h3>
+            <p>{props.description}</p>
           </div>
         </div>
       </div>
-
-      
     </div>
   );
 }
--- google-book-search/client/src/pages/Search.js
+++ google-book-search/client/src/pages/Search.js
@@ -1,50 +1,65 @@
-import React, {useRef, useState} from  "react";
+import React, { useRef, useState } from "react";
 import Jumbotron from "../components/Jumbotron";
 import axios from "axios";
 import BooksCard from "../components/BooksCard";
 
 function Searched() {
-    const [books, setBooks]= useState([]);
-    const bookRef = useRef();
-    function bookSearch(e){
-        e.preventDefault();
-        const book = bookRef.current.value;
-        const keyBook = "AIzaSyCGSsOBj1T4vGKKrQCKtGeBtOmTLEDBATo";
-        axios 
-        .get(
-            `https://www.googleapis.com/books/v1/volumes?q=${book}&key=${keyBook}`
-            
-        )
-            .then((response) => {
-                console.log(response.data.items);
-                setBooks(response.data.items);
-                
-               
+  const [books, setBooks] = useState([]);
+  const bookRef = useRef();
+  function bookSearch(e) {
+    e.preventDefault();
+    const book = bookRef.current.value;
+    const keyBook = "AIzaSyCGSsOBj1T4vGKKrQCKtGeBtOmTLEDBATo";
+    axios
+      .get(
+        `https://www.googleapis.com/books/v1/volumes?q=${book}&key=${keyBook}`
+      )
+      .then((response) => {
+        console.log(response.data.items);
+        setBooks(response.data.items);
 
-                // const results = [];
-                // for(let i = 6; i < 20; i++) {
-                //     results.push(response.data.results[i])
-                // }
-                // setNewBooks(results)
-            })
-            .catch(function (e) {
-                console.log(e);
-            });
-            
-    }
+        // const results = [];
+        // for(let i = 6; i < 20; i++) {
+        //     results.push(response.data.results[i])
+        // }
+        // setNewBooks(results)
+      })
+      .catch(function (e) {
+        console.log(e);
+      });
+  }
   return (
     <div>
       <Jumbotron />
       <form className="uk-search uk-search-navbar">
         <span uk-search-icon="true"></span>
-        <input  ref={bookRef} className="uk-search-input" type="search" placeholder="Search..." />
-        <button onClick={bookSearch} className="uk-button uk-button-default">Button</button>
+        <input
+          ref={bookRef}
+          className="uk-search-input"
+          type="search"
+          placeholder="Search..."
+        />
+        <button onClick={bookSearch} className="uk-button uk-button-default">
+          Button
+        </button>
       </form>
-      <BooksCard type="search" />
+      {books && books.length > 0
+        ? books.map((book) => {
+            return (
+              <BooksCard
+                type="search"
+                key={book.id}
+                title={book.volumeInfo.title}
+                authors={book.volumeInfo.authors[0]}
+                description={book.volumeInfo.description}
+                imageLinks={book.volumeInfo.imageLinks.smallThumbnail}
+                infoLink={book.volumeInfo.infoLink}
+              />
+            );
+          })
+        : ""}
     </div>
   );
 }
-
-
 
 export default Searched;
